02824c484ec2c6c36b2807e0591c87b28c26539c
Sanjari <sanjari.alfian.01@gmail.com>
2021-03-21 22:18:10 +0700

    First Commit

---
+++ Tugas/Junit/main/src/test/java/geoaiti/AppTest.java
@@ -0,0 +1,18 @@
+package geoaiti;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals("aa", 'a');
+    }
+}
